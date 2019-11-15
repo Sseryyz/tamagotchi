bfb5cdc8f3ec504709656df43059a364f2c36bcf
Sseryyz <12serzhj@gmail.com>
2019-11-16 01:27:21 +0200

    renamed some tags, id's.. ,added func player and interval of (big smiling)

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
+const smile = document.getElementById("tmg-smile");
+const sadSmile = document.getElementById("tmg-sadSmile");
+const bigSmile = document.getElementById("tmg-bigSmile");
 
+let feeder = function() {
+  const el = document.getElementById("tmg");
+  const { clientWidth, clientHeight } = el;
+  el.style.width = `calc(${clientWidth}px + 5px)`;
+};
 
+let player = function() {
+  smile.style.display = "none";
+  bigSmile.style.display = "block";
+  setInterval(smileFace, 5000)
+};
+let punisher = function() {
 
-let feeder = function () {
-    const el = document.getElementById("tmg");
-    const { clientWidth, clientHeight } = el;
-    el.style.width = `calc(${clientWidth}px + 5px)`;
-}
-let play = function () {
-
-}
-let punish = function () {
-
+};
+let smileFace = function () {
+  smile.style.display = "block";
+  bigSmile.style.display = "none";
 }
 document.getElementById("feed").addEventListener("click", feeder);
-document.getElementById("play").addEventListener("click", play);
-document.getElementById("panish").addEventListener("click", punish);
-
-
-
-
+document.getElementById("play").addEventListener("click", player);
+document.getElementById("panish").addEventListener("click", punisher);
