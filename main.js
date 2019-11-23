c7c5eb7f8f1d6f45a201f0aed1101d84a1450155
Sseryyz <12serzhj@gmail.com>
2019-11-23 20:49:43 +0200

    done 3rd func and change some styles

--- main.js
+++ main.js
@@ -2,24 +2,42 @@ const smile = document.getElementById("tmg-smile");
 const sadSmile = document.getElementById("tmg-sadSmile");
 const bigSmile = document.getElementById("tmg-bigSmile");
 
+setInterval(() => {
+  const el = document.getElementById("tmg");
+  const { clientWidth, clientHeight } = el;
+  el.style.width = `calc(${clientWidth}px - 5px)`;
+}, 5000);
+
 let feeder = function() {
   const el = document.getElementById("tmg");
   const { clientWidth, clientHeight } = el;
   el.style.width = `calc(${clientWidth}px + 5px)`;
+
+  tmgDisplayNone()
+  smile.style.display = "block";
 };
 
 let player = function() {
-  smile.style.display = "none";
+  tmgDisplayNone()
   bigSmile.style.display = "block";
-  setInterval(smileFace, 5000)
-};
-let punisher = function() {
 
+  setTimeout(() => {
+    tmgDisplayNone()
+    smile.style.display = "block";
+  }, 7000);
 };
-let smileFace = function () {
-  smile.style.display = "block";
+
+let punisher = function() {
+  tmgDisplayNone()
+  sadSmile.style.display = "block";
+};
+
+let tmgDisplayNone = function () {
+  smile.style.display = "none";
+  sadSmile.style.display = "none";
   bigSmile.style.display = "none";
-}
+};
+
 document.getElementById("feed").addEventListener("click", feeder);
 document.getElementById("play").addEventListener("click", player);
 document.getElementById("panish").addEventListener("click", punisher);
