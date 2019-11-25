509ecf60bf2ce997e3b6227ae5b898ba470bd34d
Sseryyz <12serzhj@gmail.com>
2019-11-25 17:09:38 +0200

    added status vaue on screen

--- main.js
+++ main.js
@@ -5,13 +5,14 @@ const bigSmile = document.getElementById("tmg-bigSmile");
 setInterval(() => {
   const el = document.getElementById("tmg");
   const { clientWidth, clientHeight } = el;
-  el.style.width = `calc(${clientWidth}px - 5px)`;
-}, 5000);
+  el.style.width = `calc(${clientWidth}px - 1px)`;
+  status()
+}, 1000);
 
 let feeder = function() {
   const el = document.getElementById("tmg");
   const { clientWidth, clientHeight } = el;
-  el.style.width = `calc(${clientWidth}px + 5px)`;
+  el.style.width = `calc(${clientWidth}px + 10px)`;
 
   tmgDisplayNone()
   smile.style.display = "block";
@@ -36,6 +37,29 @@ let tmgDisplayNone = function () {
   smile.style.display = "none";
   sadSmile.style.display = "none";
   bigSmile.style.display = "none";
+};
+
+let status = function () {
+  hungry();
+  fedUp();
+}
+
+let hungry = function () {
+  const el = document.getElementById("tmg");
+  const { clientWidth, clientHeight } = el;
+  if (el.style.width <= `calc(100px)`) {
+    document.getElementById("fed-up").style.display = "none";
+    document.getElementById("hungry").style.display = "inline-block";
+  }
+};
+
+let fedUp = function () {
+  const el = document.getElementById("tmg");
+  const { clientWidth, clientHeight } = el;
+  if (el.style.width >= `calc(100px)`) {
+    document.getElementById("hungry").style.display = "none";
+    document.getElementById("fed-up").style.display = "inline-block";
+  }
 };
 
 document.getElementById("feed").addEventListener("click", feeder);
