c15798edf28967d8bbdc0a1904a387fb2789f59c
Sseryyz <12serzhj@gmail.com>
2019-11-26 19:39:35 +0200

    added all info about tmg

--- main.js
+++ main.js
@@ -2,11 +2,16 @@ const smile = document.getElementById("tmg-smile");
 const sadSmile = document.getElementById("tmg-sadSmile");
 const bigSmile = document.getElementById("tmg-bigSmile");
 
+const feedBtn = document.getElementById("feed");
+
 setInterval(() => {
   const el = document.getElementById("tmg");
   const { clientWidth, clientHeight } = el;
   el.style.width = `calc(${clientWidth}px - 1px)`;
-  info();
+  status();
+  health();
+  weight();
+  age();
 }, 1000);
 
 let feeder = function() {
@@ -16,6 +21,7 @@ let feeder = function() {
 
   tmgDisplayNone()
   smile.style.display = "block";
+
 };
 
 let player = function() {
@@ -39,30 +45,16 @@ let tmgDisplayNone = function () {
   bigSmile.style.display = "none";
 };
 
-let info = function () {
-  fedUp();
-  hungry();
-  health();
-  // weight();
-  age();
-}
-
-let hungry = function () {
+let status = function () {
   const el = document.getElementById("tmg");
-  const { clientWidth, clientHeight } = el;
-  if (el.style.width < `calc(120px)`) {
+  let { clientWidth, clientHeight } = el;
+  if (el.style.width < `calc(180px)`) {
     document.getElementById("fed-up").style.display = "none";
     document.getElementById("hungry").style.display = "inline-block";
-  }
-};
-
-let fedUp = function () {
-  const el = document.getElementById("tmg");
-  const { clientWidth, clientHeight } = el;
-  if (el.style.width > `calc(120px)`) {
-    document.getElementById("hungry").style.display = "none";
-    document.getElementById("fed-up").style.display = "inline-block";
-  }
+  } else (
+    document.getElementById("fed-up").style.display = "inline-block",
+    document.getElementById("hungry").style.display = "none"
+  )
 };
 
 let health = function () {
@@ -70,30 +62,24 @@ let health = function () {
   const { clientWidth, clientHeight } = el;
   document.getElementById("healthy").style.display = "inline-block";
 
-  if ((el.style.width < `calc(80px)`) && (el.style.width > `calc(200px)`)) {
+  if ((el.style.width < `calc(150px)`) || (el.style.width > `calc(250px)`)) {
     document.getElementById("healthy").style.display = "none";
     document.getElementById("ill").style.display = "inline-block";
   } else document.getElementById("ill").style.display = "none";
 }
 
 let weight = function () {
-  const el = document.getElementById("tmg");
-  const { clientWidth, clientHeight } = el;
-
-  document.getElementById("weight").append(`calc(${clientWidth}px / 10px)`);
+  let el = document.getElementById("tmg");
+  let { clientWidth, clientHeight } = el;
+  document.getElementById("kg").textContent = el.clientWidth + "kg";
 }
 
 let age = function () {
-  let count = 0,
-      interval = setInterval( function () {
-        count++;
-
-        if (count === 999) {
-          clearInterval( interval);
-        }
-      }, 1000);
+  document.getElementById("year").textContent = years;
 };
+let years = 0;
+setInterval( function () { years++; }, 10000);
 
-document.getElementById("feed").addEventListener("click", feeder);
+feedBtn.addEventListener("click", feeder);
 document.getElementById("play").addEventListener("click", player);
 document.getElementById("panish").addEventListener("click", punisher);
